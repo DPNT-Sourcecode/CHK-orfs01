04274e08b14372bf2615c4b5594f8a6393c3b10d
sbx_user1060 <sbx_user1060@ip-10-128-118-26.eu-west-2.compute.internal>
2018-11-06 12:00:23 +0000

    Tue Nov 06 12:00:23 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,6 +4,34 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+
+        int total = 0;
+
+        for(Character c: skus.toCharArray()){
+            if (c == 'A') total+=50;
+            else if (c == 'B') total+=30;
+            else if (c == 'C') total+=20;
+            else if (c == 'D') total+=15;
+            else
+                return -1;
+        }
+
+        return total;
     }
+//
+//
+//    public enum SKU{
+//        A(50),B(30),C(20),D(15);
+//
+//        private final int value;
+//
+//        SKU(final int newValue){
+//            value = newValue;
+//        }
+//
+//        public int getValue() {
+//            return value;
+//        }
+//
+//    }
 }
--- src/test/java/befaster/solutions/CHK/ChkSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/ChkSolutionTest.java
@@ -1,4 +1,35 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ChkSolutionTest {
+
+    private CheckoutSolution chk;
+
+    @Before
+    public void setup(){
+        chk = new CheckoutSolution();
+    }
+
+    @Test
+    public void testCalculationOneItemEach(){
+        String basket = "ABCD";
+
+        Integer expectedTotal = 115;
+
+        assertEquals(expectedTotal, chk.checkout(basket));
+    }
+
+    @Test
+    public void testCalculationOneItem(){
+        String basket = "A";
+
+        Integer expectedTotal = 50;
+
+        assertEquals(expectedTotal, chk.checkout(basket));
+    }
+
 }
