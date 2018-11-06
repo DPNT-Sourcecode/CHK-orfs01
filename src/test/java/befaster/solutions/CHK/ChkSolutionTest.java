5c3cf7564d82e8d04a23b19deeb4156d43ce1bfe
sbx_user1060 <sbx_user1060@ip-10-128-118-26.eu-west-2.compute.internal>
2018-11-06 13:13:55 +0000

    Tue Nov 06 13:13:55 UTC 2018

--- src/test/java/befaster/solutions/CHK/ChkSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/ChkSolutionTest.java
@@ -167,4 +167,14 @@ public class ChkSolutionTest {
         assertEquals(expectedTotal, chk.checkout(basket));
     }
 
+    @Test
+    public void testNewPriceArray(){
+        String basket = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        Integer expectedTotal = 50+30+20+15+40+10+20+10+35+60+80+90+15+40+10+50+30+50+30+20+40+50+20+90+10+50;
+
+        assertEquals(expectedTotal, chk.checkout(basket));
+    }
+
+
 }
